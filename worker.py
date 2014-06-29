db9039edfcd3bb75fd54f1be0edc77f0a9624bf0
ung <qooraven@gmail.com>
2014-06-29 11:53:16 +0900

    utils.py에서 사용될 app을 import하지 않고 current app을 통해 count_words_at_url이 작동하도록

--- worker.py
+++ worker.py
@@ -1,6 +1,7 @@
 import os
 
 import redis
+from flaskr import app
 from rq import Worker, Queue, Connection
 
 listen = ['high', 'default', 'low']
@@ -11,5 +12,6 @@ conn = redis.from_url(redis_url)
 
 if __name__ == '__main__':
     with Connection(conn):
-        worker = Worker(map(Queue, listen))
-        worker.work()
+        with app.test_request_context("/"):
+            worker = Worker(map(Queue, listen))
+            worker.work()
