c760ea0f91b88d158895b1ba7698f7526e8c0533
ung <qooraven@gmail.com>
2014-06-30 15:27:33 +0900

    MAIL_USERNAME, MAIL_PASSWORD를 보안을 위해 os환경변수로 받아오도록

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+SECRET_KEY = 'devlopment key'
+MAIL_SERVER = 'smtp.gmail.com'
+MAIL_PORT = 587
+MAIL_USE_TLS = True
+MAIL_USE_SSL = False
+MAIL_USERNAME = os.environ.get('MAIL_SMTP_LOGIN', None)
+MAIL_PASSWORD = os.environ.get('MAIL_SMTP_PASSWORD', None)
+MAIL_DEFAULT_SENDER = 'qooraven@gmail.com'
