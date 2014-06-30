f401c1aa647269fece2b14361f92a6dc0b55c2cf
ung <qooraven@gmail.com>
2014-06-30 15:26:53 +0900

    add import os for os.environ.get() in utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 from flask import current_app, request
 from flask.ext.mail import Mail, Message
 import requests
+import os
 
 def count_words_at_url(url, mailAddress):
     resp = requests.get(url)
@@ -9,6 +10,5 @@ def count_words_at_url(url, mailAddress):
     msg = Message("test", recipients=["qooraven@gmail.com"])
     msg.body = str(result)
     msg.add_recipient(str(mailAddress))
-    import pdb; pdb.set_trace()
     mail.send(msg)
     return result
