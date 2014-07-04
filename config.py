44f55a27837dd08531b7fede1ee6655948fce5e4
ung <qooraven@gmail.com>
2014-07-04 16:51:43 +0900

    add FACEBOOK_APP_ID, FACEBOOK_APP_SECRET

--- config.py
+++ config.py
@@ -8,3 +8,5 @@ MAIL_USE_SSL = False
 MAIL_USERNAME = os.environ.get('MAIL_SMTP_LOGIN', None)
 MAIL_PASSWORD = os.environ.get('MAIL_SMTP_PASSWORD', None)
 MAIL_DEFAULT_SENDER = 'qooraven@gmail.com'
+FACEBOOK_APP_ID = os.environ.get('FACEBOOK_APP_ID')
+FACEBOOK_APP_SECRET = os.environ.get('FACEBOOK_APP_SECRET')
